4807ffa1ab25bbd8257afbf5760d68864168a710
Harut664 <harut.95@outlook.com>
2021-03-22 01:03:23 +0400

    ok

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
 import { StyleSheet, Switch, View, Text } from 'react-native'
+
 export default class SwitchExample extends Component {
   state = {
-    switchValue: true  };
-  render() {    return (
+    switchValue: true
+  };
+
+  render() {
+    return (
+      
      <View style={styles.body}>
           <View style={{
             backgroundColor: (this.state.switchValue ? 'white' : 'black'),
-          height: '100%', width: '100%', alignItems: 'center',          }}>
+            height: '100%', width: '100%',  alignItems: 'center',
+          }}>
             <Switch
               value={this.state.switchValue}
-              onValueChange={(switchValue) => this.setState({ switchValue })} />      </View>     </View>    );
+              onValueChange={(switchValue) => this.setState({ switchValue })} />
+
+          </View>
+     </View>
+    );
   }
-}const styles = StyleSheet.create({
+}
+const styles = StyleSheet.create({
   body:{
     justifyContent: 'center',
     alignItems: 'center',
